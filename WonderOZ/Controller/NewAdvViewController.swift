248fbdd7d1412556448d20a286891ad98f13d2e0
WoksniT <s3459799@student.rmit.edu.au>
2018-01-23 19:01:28 +1100

    Update New Adv script

--- WonderOZ/Controller/NewAdvViewController.swift
+++ WonderOZ/Controller/NewAdvViewController.swift
@@ -10,7 +10,18 @@ import UIKit
 
 class NewAdvViewController: UIViewController {
 
+    var adventureLst = AdventureData.adventureInstance.adventureMap
+    var adventure: Adventure?
     
+    @IBOutlet weak var tit: UITextField!
+    @IBOutlet weak var location: UITextField!
+    @IBOutlet weak var cate: UITextField!
+    @IBOutlet weak var Descript: UITextView!
+ 
+    @IBAction func saveFunc(_ sender: Any) {
+        //adventure = Adventure((adventureLst?.count)! + 1, nil)
+        adventureLst?.updateValue(Adventure(itemId: (adventureLst?.count)! + 1, category: Category.camping), forKey: (adventureLst?.count)! + 1)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
